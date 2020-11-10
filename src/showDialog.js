aff59e321e4a270e56266141af416485470f9f8f
RAINeko256 <reirain256@gmail.com>
2020-11-10 17:02:04 +0900

    確認ダイアログの雛形

---
+++ src/showDialog.js
@@ -0,0 +1,27 @@
+import React , {useState,useEffect} from 'react';
+import {Button , Dialog , DialogActions , DialogContent} from '@material-ui/core';
+
+const showDialog = () => {
+	const reconfmsg="打刻します。よろしいですか？"
+	const [open,setOpen]= React.useState(false);
+
+	return(
+		<Dialog
+			open={open}
+		>
+			<DialogContent>
+				{msg}
+			</DialogContent>
+			<DialogActions>
+				<Button onclick={} color="primary">
+					打刻
+				</Button>
+			</DialogActions>
+		</Dialog>
+		);
+}
+
+export default showDialog;
+
+/*<p>この時刻で打刻します？よろしいですか？</p>
+			<Button variant="contained" color="primary">打刻</Button>*/
